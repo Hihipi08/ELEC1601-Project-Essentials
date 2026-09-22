5da7c02144b21491d5ebd3cdc0fe3e95c0257548
liammorony-commits <liammorony@gmail.com>
2026-09-22 17:23:35 +1000

    Added dead end

--- Forward_right_and_left_turn.cpp
+++ Forward_right_and_left_turn.cpp
@@ -1,5 +1,4 @@
-// This code makes robot go forward and then turn right and left if there is a gap in the wall
-// Still needs code to detect a wall in front of the robot
+// this code includes going forwards, ideal 90 degree right and left turn, dead end and does 180 degree turn
 #include <Servo.h>
 
 Servo servoLeft;
@@ -23,6 +22,9 @@ int ir_valR = 0;
 int distanceL = 0;
 int distanceF = 0;
 int distanceR = 0;
+int Sensor_LED_Left = A2;
+int Sensor_LED_Front = A1;
+int Sensor_LED_Right = A0;
 
 void setup()
 {
@@ -33,50 +35,76 @@ void setup()
   
   pinMode(IR_LED_Right, OUTPUT);
   pinMode(IR_LED_Left, OUTPUT);
+  pinMode(IR_LED_Front, OUTPUT);
   pinMode(Sensor_Left, INPUT);
   pinMode(Sensor_Right, INPUT);
-  delay(5000);
+  pinmode(Sensor_Front, INPUT);
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1500);
+  delay(2000); //just so robot is stationary when placing it in the maze
 }
 void loop()
 {
-  if (irDetectFrequencyRight() == 1 && irDetectFrequencyLeft() == 1)
+  if (irDetectFrequencyRight() == 1 && irDetectFrequencyLeft() == 1 && irDetectFrequencyLeft() == 0) //forwards
   {
     servoLeft.writeMicroseconds(1568); 
     servoRight.writeMicroseconds(1432);
-    
-    Serial.println("going forward");
+    digitalWrite(Sensor_LED_Left, HIGH);
+    digitalWrite(Sensor_LED_Right, HIGH);
+    digitalWrite(Sensor_LED_Front, LOW);
   }
 
-  else if (irDetectFrequencyRight() == 0 && irDetectFrequencyLeft() == 0)
+  else if (irDetectFrequencyRight() == 0 && irDetectFrequencyLeft() == 0 && irDetectFrequencyLeft() == 0) //forwards
   {
     servoLeft.writeMicroseconds(1568);
     servoRight.writeMicroseconds(1432);
-    
-    Serial.println("going forward");
+    digitalWrite(Sensor_LED_Left, LOW);
+    digitalWrite(Sensor_LED_Right, LOW);
+    digitalWrite(Sensor_LED_Front, LOW);
   }
     
   
-  else if (irDetectFrequencyLeft() == 0 && irDetectFrequencyRight() == 1) //left turn
+  else if (irDetectFrequencyLeft() == 0 && irDetectFrequencyRight() == 1 && irDetectFrequencyLeft() == 0) //left turn
     {
       delay(900);
-      servoLeft.writeMicroseconds(1550);
+      servoLeft.writeMicroseconds(1550); //turn
       servoRight.writeMicroseconds(1550);
-
-      Serial.println("turning left");
+      digitalWrite(Sensor_LED_Right, HIGH);
+      digitalWrite(Sensor_LED_Left, LOW);
+      digitalWrite(Sensor_LED_Front, LOW);
       delay(830);
-      servoLeft.writeMicroseconds(1568);
+      servoLeft.writeMicroseconds(1568); //moves forwards before checking where it is again
       servoRight.writeMicroseconds(1432);
       delay(500);
     }
 
-    else if (irDetectFrequencyRight() == 0 && irDetectFrequencyLeft() == 1) //right turn
+    else if (irDetectFrequencyRight() == 0 && irDetectFrequencyLeft() == 1 && irDetectFrequencyLeft() == 0) //right turn
     {
       delay(900);
-      servoLeft.writeMicroseconds(1450);
+      servoLeft.writeMicroseconds(1450); //turn
       servoRight.writeMicroseconds(1450);
-      Serial.println("turning right");
+      digitalWrite(Sensor_LED_Left, HIGH);
+      digitalWrite(Sensor_LED_Right, LOW);
+      digitalWrite(Sensor_LED_Front, LOW);
       delay(780);
-      servoLeft.writeMicroseconds(1568);
+      servoLeft.writeMicroseconds(1568); //moves forwards before checking where it is again
+      servoRight.writeMicroseconds(1432);
+      delay(500);
+    }
+
+    // Dead end, does 180 turn then moves forward
+    if (irDetectFrequencyFront() == 1 && irDetectFrequencyRight() == 1 && irDetectFrequencyLeft() == 1)
+    {
+      servoLeft.writeMicroseconds(1500); //stops for half a second
+      servoRight.writeMicroseconds(1500);
+      digitalWrite(Sensor_LED_Right, HIGH);
+      digitalWrite(Sensor_LED_Left, HIGH);
+      digitalWrite(Sensor_LED_Front, HIGH);
+      delay(500);
+      servoLeft.writeMicroseconds(1550); //180 degree turn
+      servoRight.writeMicroseconds(1550);
+      delay(1660);
+      servoLeft.writeMicroseconds(1568); // goes forwards for half a second
       servoRight.writeMicroseconds(1432);
       delay(500);
     }
@@ -101,4 +129,14 @@ int irDetectFrequencyRight()
   noTone(IR_LED_Right);
   delay(1);
   return ir_valR;
+}
+
+int irDetectFrequencyFront()
+{
+  tone(IR_LED_Front, 38000);
+  delay(1);
+  ir_valR = digitalRead(Sensor_Front);
+  noTone(IR_LED_Front);
+  delay(1);
+  return ir_valF;
 }
